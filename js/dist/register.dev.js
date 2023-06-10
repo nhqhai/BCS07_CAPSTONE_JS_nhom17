d2f332d83508c2efa901eae9dd6566f3e633fcc0
nhqhai <nhqhai@gmail.com>
2023-06-10 08:54:08 +0700

    first

---
+++ js/dist/register.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function registerKhach() {
+  var promise = axios({
+    url: 'https://shop.cyberlearn.vn/api/Users/signup',
+    method: 'POST'
+  });
+  promise.then(function (res) {
+    renderKhach(res.data);
+  });
+  promise["catch"](function (err) {});
+}
+
+registerKhach();
