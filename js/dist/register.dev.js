108dae267ea5956acd600046a4fc7388ff8c162d
Nguyễn Hoàng Quốc Hải <112468220+nhqhai@users.noreply.github.com>
2023-06-15 15:11:58 +0700

    register done

--- js/dist/register.dev.js
+++ js/dist/register.dev.js
@@ -1,14 +1,29 @@
 "use strict";
 
-function registerKhach() {
+var form = document.querySelector('.contact-form');
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var email = document.querySelector('#email').value;
+  var name = document.querySelector('#name').value;
+  var password = document.querySelector('#password').value;
+  var phone = document.querySelector('#phone').value;
+  var gender = document.querySelector('input[name="gender"]:checked').value === 'male' ? true : false;
   var promise = axios({
     url: 'https://shop.cyberlearn.vn/api/Users/signup',
-    method: 'POST'
+    method: 'POST',
+    data: {
+      email: email,
+      name: name,
+      password: password,
+      phone: phone,
+      gender: gender
+    },
+    responseType: 'json'
   });
-  promise.then(function (res) {
-    renderKhach(res.data);
+  promise.then(function (response) {
+    console.log(response.data.content);
+    form.reset();
+  })["catch"](function (error) {
+    console.error(error);
   });
-  promise["catch"](function (err) {});
-}
-
-registerKhach();
+});
--- js/register.js
+++ js/register.js
@@ -1,13 +1,29 @@
-function registerKhach(){
-var promise = axios({
-    url: 'https://shop.cyberlearn.vn/api/Users/signup',
-    method: 'POST',
-});
-promise.then(function (res) {
-    renderKhach(res.data);
-});
-promise.catch(function (err) {
-});
-}
+var form = document.querySelector('.contact-form');
 
-registerKhach();
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    var email = document.querySelector('#email').value;
+    var name = document.querySelector('#name').value;
+    var password = document.querySelector('#password').value;
+    var phone = document.querySelector('#phone').value;
+    var gender = document.querySelector('input[name="gender"]:checked').value === 'male' ? true : false; 
+    var promise = axios({
+      url: 'https://shop.cyberlearn.vn/api/Users/signup',
+      method: 'POST',
+      data: {
+        email: email,
+        name: name,
+        password: password,
+        phone: phone,
+        gender: gender
+      },
+      responseType: 'json'
+    });
+    promise.then(function(response) {
+      console.log(response.data.content);
+      form.reset();
+    }).catch(function(error) {
+      console.error(error);
+    });
+  });
+  
